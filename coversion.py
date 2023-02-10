201f6c4eb5ad68c68f6f264b06e1d030f1384c75
Sagar Regmi <107130282+Saakabhai@users.noreply.github.com>
2023-02-10 17:52:43 +0000

    modified:   coversion.py

--- coversion.py
+++ coversion.py
@@ -9,25 +9,49 @@ for image in dataset:
         colormappedNDVI = cv.applyColorMap(scaledNDVI, cv.COLORMAP_JET)
         imagePlot.set_data(colormappedNDVI)
 
-## Use the following conversion to convert a multispectral image to a CCCI image
+## Another method for NDVI (Normalized Difference Vegetation Index)
+## Source: Rouse 1974
+
+  NDVI = (R800nm - R670nm)/(R800nm + R670nm)
+
+## Use the following conversion to convert a multispectral image to a CCCI (canopy chlorophyll concentration index) image
+## Source: Perry 2017
+  CCCI = (NDRE - NDREmin)/(NDREmax - NDREmin)
 
 
 ## Use the following conversion to convert a multispectral image to a GLI (green leaf index) image
 
     GLI = (2Green - Red - Blue) / (2Green + Red + Blue)
 
-## Use the following conversion to convert a multispectral image to a GNDVI image
+## Use the following conversion to convert a multispectral image to a GNDVI (Green Normalized Difference Vegetation Index) image
+## source Gitelson 1996
 
-## Use the following conversion to convert a multispectral image to a NDRE image
+  GNDVI = (R_800nm - R_550nm)/(R_800nm + R_550nm)
+
+## Another formula for GNDVI is
+
+    GNDVI = (NIR - GREEN)/(NIR + GREEN)
+
+
+## Use the following conversion to convert a multispectral image to a NDRE (Normalized Difference Red-edge Vegetation Index) image
+## Source Fitzgerald 2010
+## Note a paper citing most of these indexes in saved on the project literature review folder
+
+  NDRE = (R800nm - R720nm)/(R800nm + R720nm)
 
 ## Use the following conversion to convert a multispectral image to a SIPI image
 
-## Use the following conversion to convert a multispectral image to a EXGI image
+## Use the following conversion to convert a multispectral image to a EXGI (Excess Green Vegetation Index) image
+## Source: Mengmeng Du 2017
+  ExG of EXGI = 2G - B - R
 
 ## Use the following conversion to convert a multispectral image to a VARI (Visible Atmospherically Resistant Index) image
 
   VARI = (Green - Red)/(Green + Red - Blue)
 
-## Use the following conversion to convert a multispectral image to a TGI image
+## another method for VARI based on Gitelson 2002
+  VARI = (R550nm - R670nm)/(R550nm + R670nm)
+
+## Use the following conversion to convert a multispectral image to a TGI (Triangular Greenness Index) image
 
 ## Use the following conversion to convert a multispectral image to a AVRI image
